4b7457ee998a6305ad44dfa04085cde6ed4a1d26
jrfnl <jrfnl@users.noreply.github.com>
2022-08-19 11:15:35 +0200

    Tests: move constants to base test class

--- tests/ForbidDynamicProperties/ForbidDynamicPropertiesTest.php
+++ tests/ForbidDynamicProperties/ForbidDynamicPropertiesTest.php
@@ -22,17 +22,6 @@ final class ForbidDynamicPropertiesTest extends TestCase
      * - Add parent-child-grandchild test set with variations
      */
 
-    const TEST_VALUE_1 = 'Testing-1-2-3';
-
-    const ERR_NO_ACCESS = 'PHP Error: no access';
-    const ERR_NO_ACCESS_MSG_REGEX = '`Cannot access (?:private|protected) property`';
-
-    const ERR_UNDEFINED = 'PHP Error: undefined property';
-    const ERR_UNDEFINED_MSG = 'Undefined property: ';
-
-    const ERR_OUTOFBOUNDS = 'Exception: out of bounds';
-    const ERR_OUTOFBOUNDS_MSG = 'Dynamic properties are not supported for class ';
-
     /**
      *
      * @dataProvider dataPropertyAccessFromOutsideClassPhpNative
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,15 @@ use Yoast\PHPUnitPolyfills\TestCases\TestCase as Polyfill_TestCase;
 
 abstract class TestCase extends Polyfill_TestCase
 {
+    const TEST_VALUE_1 = 'Testing-1-2-3';
+    const TEST_VALUE_2 = 12345;
+
+    const ERR_NO_ACCESS = 'PHP Error: no access';
+    const ERR_NO_ACCESS_MSG_REGEX = '`Cannot access (?:private|protected) property`';
+
+    const ERR_UNDEFINED = 'PHP Error: undefined property';
+    const ERR_UNDEFINED_MSG = 'Undefined property: ';
+
+    const ERR_OUTOFBOUNDS = 'Exception: out of bounds';
+    const ERR_OUTOFBOUNDS_MSG = 'Dynamic properties are not supported for class ';
 }
